1d8656942cc86cf9b71a1f195436f115266d2ec1
Monir Hossain <mknishad1@ymail.com>
2016-06-10 00:37:25 +0600

    Initial commit. design done

---
+++ app/src/main/java/com/example/nishad/myfirstcalculator/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.nishad.myfirstcalculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView display;
+    private Button btnZero;
+    private Button btnOne;
+    private Button btnTwo;
+    private Button btnThree;
+    private Button btnFour;
+    private Button btnFive;
+    private Button btnSix;
+    private Button btnSeven;
+    private Button btnEight;
+    private Button btnNine;
+    private Button btnPlus;
+    private Button btnMinus;
+    private Button btnMultiply;
+    private Button btnDivide;
+    private Button btnPoint;
+    private Button btnEqual;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        display = (TextView) findViewById(R.id.display);
+        btnZero = (Button) findViewById(R.id.btnZero);
+        btnOne = (Button) findViewById(R.id.btnOne);
+        btnTwo = (Button) findViewById(R.id.btnTwo);
+        btnThree = (Button) findViewById(R.id.btnThree);
+        btnFour = (Button) findViewById(R.id.btnFour);
+        btnFive = (Button) findViewById(R.id.btnFive);
+        btnSix = (Button) findViewById(R.id.btnSix);
+        btnSeven = (Button) findViewById(R.id.btnSeven);
+        btnEight = (Button) findViewById(R.id.btnEight);
+        btnNine = (Button) findViewById(R.id.btnNine);
+        btnPlus = (Button) findViewById(R.id.btnPlus);
+        btnMinus = (Button) findViewById(R.id.btnMinus);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnDivide = (Button) findViewById(R.id.btnDivide);
+        btnPoint = (Button) findViewById(R.id.btnPoint);
+        btnEqual = (Button) findViewById(R.id.btnEqual);
+    }
+}
